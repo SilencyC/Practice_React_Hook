f031610eae6a3f4a54e5ff172426aea6e6ad08bb
chengbin <cheng_bin122@163.com>
2020-05-11 13:05:06 +0800

    handle useReducer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import UseReducerDemo from './components/UseReducerDemo';
 
 const App = () => {
+  const dataFetchReducer = (state, action) => {};
+  const defalutState = {
+    data: '',
+    isLoading: '',
+    isError: '',
+  };
+  const [state, dispatch] = useReducer(dataFetchReducer, defalutState);
+  console.log(state);
+  
   return (
     <div>
       <UseReducerDemo></UseReducerDemo>
--- src/components/UseReducerDemo.js
+++ src/components/UseReducerDemo.js
@@ -1,18 +1,81 @@
-import React, { useState, useEffect, Fragment } from 'react';
+import React, { useState, useEffect, Fragment, useReducer } from 'react';
 import axios from 'axios';
 
 export default function UseReducer() {
   const [query, setQuery] = useState('redux');
-  const [
-    { data, isLoading, isError },
-    setUrl,
-  ] = useCustomHook('https://hn.algolia.com/api/v1/search?query=redux', {
-    hits: [],
-  });
+  // const [{ data, isLoading, isError }, setUrl] = useCustomHook(
+  //   'https://hn.algolia.com/api/v1/search?query=redux',
+  //   {
+  //     hits: [],
+  //   }
+  // );
+
+  const dataFetchReducer = (state, action) => {
+    switch (action.type) {
+      case 'SET_DATA':
+        return {
+          ...state,
+          data: action.payload,
+          isLoading: false,
+        };
+      case 'FETCH_INIT':
+        return {
+          ...state,
+          isLoading: true,
+          isError: false,
+        };
+      case 'FETCH_FAILURE':
+        return {
+          ...state,
+          isLoading: false,
+          isError: true,
+        };
+      default:
+        return { ...state };
+    }
+  };
+
+  //useReducer
+  const useDataApi = (initialUrl, initialData) => {
+    const [url, setUrl] = useState(initialUrl);
+
+    const defalutState = {
+      data: initialData,
+      isLoading: false,
+      isError: false,
+    };
+
+    const [state, dispatch] = useReducer(dataFetchReducer, defalutState);
+
+    useEffect(() => {
+      const fatchData = async () => {
+        dispatch({ type: 'FETCH_INIT' });
+        try {
+          const result = await axios(url);
+          dispatch({ type: 'SET_DATA', payload: result.data });
+        } catch (event) {
+          dispatch({ type: 'FETCH_FAILURE' });
+        }
+      };
+      fatchData();
+    }, [url]);
+
+    return [state, setUrl];
+  };
+
+  const [state, setUrl] = useDataApi(
+    'https://hn.algolia.com/api/v1/search?query=redux',
+    {
+      hits: [],
+    }
+  );
+
+  console.log('state::', state);
 
   return (
     <Fragment>
-      <form
+      
+      {/* <form
         onSubmit={(event) => {
           event.preventDefault();
           setUrl(`https://hn.algolia.com/api/v1/search?query=${query}`);
@@ -39,11 +102,41 @@ export default function UseReducer() {
             );
           })}
         </ul>
+      )} */}
+
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          setUrl(`https://hn.algolia.com/api/v1/search?query=${query}`);
+        }}
+      >
+        <input
+          type="text"
+          value={query}
+          onChange={(event) => setQuery(event.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      {state.isError && <div>Something went wrong ...</div>}
+
+      {state.isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <ul>
+          {state.data.hits.map((item) => {
+            return (
+              <li key={item.objectID}>
+                <a href={item.url}>{item.title}</a>
+              </li>
+            );
+          })}
+        </ul>
       )}
     </Fragment>
   );
 }
 
+//only useState
 const useCustomHook = (initialUrl, initialData) => {
   const [data, setData] = useState(initialData);
   const [url, setUrl] = useState(initialUrl);
